dd311a2f57e602aa24a21813ddae238d787e130b
Ekrash Zahid <mekrashzahid@gmail.com>
2025-01-25 17:15:52 +0500

    Only see the java file ignore class Files

--- src/Bankproess.java
+++ src/Bankproess.java
@@ -1,11 +1,14 @@
 public class Bankproess {
     PaymentMethods paymentMethods;
-    public void BankProcess(){}
-    public void SetStrategy(PaymentMethods pb){
-        this.paymentMethods=pb;
-}
-public void pay(double amount)
-{
-    paymentMethods.pay(amount);
-}
+
+    public void BankProcess() {
+    }
+
+    public void SetStrategy(PaymentMethods pb) {
+        this.paymentMethods = pb;
+    }
+
+    public void pay(double amount) {
+        paymentMethods.pay(amount);
+    }
 }
